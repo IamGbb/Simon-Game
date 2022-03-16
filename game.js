75450a6eddd918928444975464cf2ffbbf74d042
Gilad <giladmta@gmail.com>
2022-03-16 15:09:54 +0200

    click instead of keydown

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).input(function() {
+$(document).click(function() {
   if (!started) {
     $('#level-title').text("level " + level);
     nextSequence();
